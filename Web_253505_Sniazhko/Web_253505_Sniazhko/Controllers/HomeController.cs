0e9226859617afa44a2a4dc19139165449818068
Tekken <maks.kreet@mail.ru>
2024-09-05 21:30:46 +0300

    Added 1st lab

---
+++ Web_253505_Sniazhko/Web_253505_Sniazhko/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_253505_Sniazhko.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lab1()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web_253505_Sniazhko/Web_253505_Sniazhko/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Web_253505_Sniazhko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class Book { public int BookId { get; set; } public string Name { get; set; } public string Author { get; set; } }
+}
