0532087451e125c35d87ade8a9f84f663db5ec23
Tekken <maks.kreet@mail.ru>
2024-10-24 17:59:38 +0300

    Added 10th lab

--- Web_253505_Sniazhko/Web_253505_Sniazhko.UI/Program.cs
+++ Web_253505_Sniazhko/Web_253505_Sniazhko.UI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Serilog;
 using Web_253505_Sniazhko.UI;
 using Web_253505_Sniazhko.UI.ApiInteraction;
 using Web_253505_Sniazhko.UI.ApiInteraction.Services;
@@ -13,16 +14,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 var uriData = builder.Configuration.GetSection("UriData").Get<UriData>();
 
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorComponents();
 builder.RegisterCustomServices(uriData);
 
+builder.Services.AddSerilog();
 
+var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
 
-
+// Log.Logger.Information("Hello, world!");
 
 var app = builder.Build();
 
@@ -53,5 +64,8 @@ app.MapControllerRoute(
     );
 
 app.MapRazorPages();
+
+app.UseMiddleware<LoggingMiddleware>();
+
 
 app.Run();
